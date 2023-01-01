04a9bdfe3e090d0c1688f866a8c3957f47ed0421
brendonrodri <rodriguesbrendon861@gmail.com>
2022-12-31 23:55:25 -0300

    removido bottão e input, adicionado useEffect

--- src/components/apiComponent/index.js
+++ src/components/apiComponent/index.js
@@ -1,28 +1,29 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
 const ApiContainer = styled.section`
     display: flex;
-    flex-direction: column;
     align-items: center;
+    flex-wrap: wrap;
 `
 const Img = styled.img`
-    width: 70%;
-    margin: 0 auto;
+    width: 30%;
 `
-
-export default function ApiComponent (){
-const [img, setImg] = useState()
-    const getApi = ()=>{
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=ZJ64BLB4l4yTmzDr5wRNb6E7GDAWSlO9Xw6MOdhY`).then((response)=>{
-            console.log(response.data.hdurl)
-            setImg(response.data.hdurl)
+export default function ApiComponent() {
+    const [apidata, setApidata] = useState([])
+    useEffect(()=>{
+        getApi()
+    },[])
+    const getApi = () => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=ZJ64BLB4l4yTmzDr5wRNb6E7GDAWSlO9Xw6MOdhY&count=50`).then((response) => {
+            setApidata(response.data)
         })
     }
-    return(
+    return (
         <ApiContainer>
-            <button onClick={()=>{getApi()}}>Buscar</button>
-            <Img src={img} alt="asasassasa" />
+            {apidata.map((item)=>(
+                    <Img src={item.url} alt={item.title} />
+            ))}
         </ApiContainer>
     )
 }
